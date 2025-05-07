e04622280c7e4c834984b780dcc158659de1b4b4
fastestsunil <sunil141292@gmail.com>
2025-05-07 12:10:22 +0530

    Update order model to allow guest checkouts and modify order creation logic to handle guest orders appropriately

--- model/Order.js
+++ model/Order.js
@@ -5,7 +5,11 @@ const orderSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true,
+      required: false,
+    },
+    isGuestOrder: {
+      type: Boolean,
+      default: false,
     },
     cart: [{}],
     name: {
