b080cf8027e42775e3491fb27eb6a660b2fc8925
fastestsunil <sunil141292@gmail.com>
2025-11-09 19:40:02 +0530

    chore: remove outdated CORS origin from configuration
    - Removed the origin 'https://ewo-admin.globalinnovations.co.in' from the CORS settings to eliminate redundancy and ensure the configuration reflects current access requirements.

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ app.use(
       'http://localhost:4000',
       'https://admin.eastwestoffroad.com',
       'https://ewo-admin.vercel.app',
-      'https://ewo-admin.globalinnovations.co.in'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
