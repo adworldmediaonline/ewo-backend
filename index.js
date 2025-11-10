45207c285e10e0658e8c69c89af4d79d39015d9d
fastestsunil <sunil141292@gmail.com>
2025-11-10 12:04:49 +0530

    chore: add new CORS origin for admin-v2 access
    - Included the origin 'https://admin-v2.eastwestoffroad.com' in the CORS configuration to support the new admin interface, enhancing cross-origin request handling.

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ app.use(
       'http://localhost:4000',
       'https://admin.eastwestoffroad.com',
       'https://ewo-admin.vercel.app',
+      'https://admin-v2.eastwestoffroad.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
